50fdde65aa7ea073a2cc86b5ef8342e4fce06edb
Pablo Medina Moreno <alu0101481449@ull.edu.es>
2025-05-04 20:51:45 +0100

    jwt test

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,7 @@ export const NODE_ENV = process.env.NODE_ENV;
 export const PORT = process.env.PORT;
 export const BACKEND_URL = process.env.BACKEND_URL;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_SECRET = process.env.JWT_SECRET || 'default_jwt_secret';
 
 export const AWS_REGION = process.env.AWS_REGION;
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
@@ -23,5 +23,5 @@ export const DATABASE_URL = process.env.NODE_ENV === 'test'
     ? process.env.DATABASE_URL
     : process.env.DATABASE_URL_LOCAL;
 
-export const EMAIL_FROM = process.env.EMAIL_FROM;
-export const EMAIL_PASS = process.env.EMAIL_PASS;
+export const EMAIL_FROM = process.env.EMAIL_FROM || ' '
+export const EMAIL_PASS = process.env.EMAIL_PASS || ' '
